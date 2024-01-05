64a1d5871c5df5357480509c79449496d5f2c82e
lin <shower112024@gmail.com>
2024-01-05 17:57:32 +0800

    改變大小寫

--- change_case.py
+++ change_case.py
@@ -1,2 +1,13 @@
 def change_case(input_str, case):
-    pass
+    if case == "upper":
+        return input_str.upper()
+    elif case == "lower":
+        return input_str.lower()
+    else:
+        return "請輸入upper或lower~"
+    
+user_string = input("請輸入你字串：")
+change  = input("請輸入upper或lower:")
+
+change_string = change_case(user_string , change)
+print("已更改：",change_string)
