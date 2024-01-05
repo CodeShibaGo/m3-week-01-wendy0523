d0d4983fcd487b3abea6368c5615a4309be0ed67
lin <shower112024@gmail.com>
2024-01-05 17:24:14 +0800

    刪除指定字

--- disemvowel.py
+++ disemvowel.py
@@ -1,2 +1,16 @@
 def disemvowel(s):
-    pass
+    return s.translate(str.maketrans("","","aeiou"))
+
+#string.translate(table)可以指定字串中的每個字元替換,刪除,或新增
+#string.maketrans(x[,y[,z]])
+
+
+string = "appleisgoodtoyou" 
+del_string = disemvowel(string)
+print(del_string)
+
+# delete = "aeiou"
+# delete_string = "".join(x for x in string if x not in delete)
+# print(delete_string)
+#列表推導式 
+#[x for x in iterable if expression]
