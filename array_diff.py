15b4e98e2e9616415c0e4f7a75f76e38f15d3e26
lin <shower112024@gmail.com>
2024-01-06 09:27:52 +0800

    array different

--- array_diff.py
+++ array_diff.py
@@ -1,2 +1,11 @@
 def array_diff(a, b):
-    pass
+    return [x for x in a if x not in b]
+
+first_input = (input("請輸入一列數字：（空格分開）"))
+second_input =(input("請輸入一列數字：（空格分開）"))
+
+a = list(map(int,first_input.split()))
+b = list(map(int,second_input.split()))
+
+result = array_diff(a,b)
+print("差異",result)
