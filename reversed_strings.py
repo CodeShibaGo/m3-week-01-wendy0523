31abca3e5b75027680b3126176b02aaf17400376
lin <shower112024@gmail.com>
2024-01-06 08:45:30 +0800

    reversed strings

--- reversed_strings.py
+++ reversed_strings.py
@@ -1,2 +1,10 @@
 def reverse_string(s):
-    pass
+    return s[::-1]
+
+string = "Hello, World!"
+reversed_string = distinct(string)
+print(reversed_string) 
+
+#reversed_string1 = ''.join(reversed(string))
+#print(reversed_string1)
+#!dlroW ,olleH
