1151cd86fe0df51c26fd4cc3fef0907486647719
lin <shower112024@gmail.com>
2024-01-06 08:25:36 +0800

    even or odd

--- even_or_odd.py
+++ even_or_odd.py
@@ -1,2 +1,11 @@
 def even_or_odd(number):
-    pass
+    if number % 2 == 0:
+        return "{0}是Even ".format(number)
+    else:
+        return "{0}是Odd ".format(number)
+    
+user_input = int(input("請輸入一個整數："))
+result_input = even_or_odd (user_input)
+print(result_input)
+
+
