6ab707f970d75529f1266aa46ef919439cecebc2
lin <shower112024@gmail.com>
2024-01-06 08:44:10 +0800

    更新

--- remove_duplicates_from_list.py
+++ remove_duplicates_from_list.py
@@ -1,10 +1,2 @@
 def distinct(seq):
-    return seq[::-1]
-
-string = "Hello, World!"
-reversed_string = distinct(string)
-print(reversed_string) 
-
-#reversed_string1 = ''.join(reversed(string))
-#print(reversed_string1)
-#!dlroW ,olleH
+    pass 
