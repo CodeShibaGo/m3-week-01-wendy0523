ca7541832f357e15824a311164bf204136fcbb4b
lin <shower112024@gmail.com>
2024-05-03 16:49:41 +0800

    modify unique_in_order

--- unique_in_order.py
+++ unique_in_order.py
@@ -1,2 +1,9 @@
 def unique_in_order(iterable):
-    pass
+    if not iterable:
+        return []
+    result = [iterable[0]]
+    
+    for x in range(1,len(iterable)):
+        if iterable[x] != result[-1]:
+            result.append(iterable[x])
+    return result
